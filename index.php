fd8b499395a1816e61ef068247a080645cb6744e
zhangzl <zhangzl@ename.com>
2014-08-26 10:28:13 +0800

    aaa

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	echo 'this is first code';
 	echo '123';
-	echo 'create a new brach is quick';
+	echo 'create a new brach is quick and simple';
 	echo 'this is the branch name feature1';
 ?>
